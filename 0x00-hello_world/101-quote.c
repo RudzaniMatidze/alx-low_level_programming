4066172309ada241bc513e87522079e43b76a2df
RudzaniMatidze <rdzmatidze@gmail.com>
2023-10-05 16:13:52 -0700

    101-qoute.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
-#include <unistd.h>
+#include <stdio.h>
+#incluce <unistd.h>
 /**
  * main - A program that prints a line to standard error
  * Return: 1 (success)
  */
 int main(void)
 {
-	fput("Write a C program that prints exactly and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", stdout);
 	return (1);
 }
