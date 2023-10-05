62102bca17f1e6711fb64f7cbeae6214cdac8429
RudzaniMatidze <rdzmatidze@gmail.com>
2023-10-05 16:05:31 -0700

    101-qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fput("Write a C program that prints exactly and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n");
+	fput("Write a C program that prints exactly and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", stdout);
 	return (1);
 }
