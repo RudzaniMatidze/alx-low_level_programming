a83df6cf259d3f5ae794eb5a513bcfd11a6b777f
RudzaniMatidze <rdzmatidze@gmail.com>
2023-10-05 17:36:18 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,13 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <unistd>
+
 /**
  * main - Entry point
- * Description: Print a message to standard error
- * Return: 1 (error)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	 ssize_t len;
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-    len = 59;
-    if (write(STDERR_FILENO, message, len) != len)
-        return (1);
-
-    return (1);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	return (1);
 }
