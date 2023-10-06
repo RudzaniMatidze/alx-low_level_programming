f78934916c858cf240ad191f1bd523a66bb85f25
RudzaniMatidze <rdzmatidze@gmail.com>
2023-10-06 15:09:57 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: always 0
+ */
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(d <= 'z') {
+			putchar (c);
+			c++;
+		}
+	while
+		(d <= 'z') {
+			putchar (d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
