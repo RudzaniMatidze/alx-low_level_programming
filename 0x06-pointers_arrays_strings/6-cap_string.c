6b091c6feb9d38413f53b06b39adc6e28b55fec3
RudzaniMatidze <rdzmatidze@gmail.com>
2023-10-19 10:29:01 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,36 @@
 #include "main.h"
-
 /**
- * *cap_string - Capitalizes all words of a string.
- * @str: the string to be capitalized
+ * cap_string - capitalizes the first letter of a word in a string.
+ * separators of words are:  space, tabulation,
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }.
+ * @n: pointer to string.
  *
+ * Return: pointer to s.
+ */
+char *cap_string(char *n)
+{
+	int count;
+
+	/*  scan through string */
+	count = 0;
+	while (n[count] != '\0')
+	{/* if next character after count is a char , capitalise it */
+		if (n[0] >= 97 && n[0] <= 122)
+		{
+			n[0] = n[0] - 32;
+		}
+		if (n[count] == ' ' || n[count] == '\t' || s[count] == '\n'
+				|| n[count] == ',' || n[count] == ';' || n[count] == '.'
+				|| n[count] == '.' || n[count] == '!' || n[count] == '?'
+				|| n[count] == '"' || n[count] == '(' || n[count] == ')'
+				|| n[count] == '{' || n[count] == '}')
+		{
+			if (n[count + 1] >= 97 && n[count + 1] <= 122)
+			{
+				n[count + 1] = n[count + 1] - 32;
+			}
+		}
+		count++;
+	}
+	return (n);
+}
