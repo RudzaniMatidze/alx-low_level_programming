6953f4718b48b9171286406b3a6f74e897f83b31
RudzaniMatidze <rdzmatidze@gmail.com>
2023-10-25 04:16:38 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,15 +2,17 @@
 
 /**
  * _puts_recursion - A function that prints a string.
- * @s: A string
+ * @s: A sttring.
  * Return: void
  */
 void _puts_recursion(char *s)
 {
-	while (*s != '\0')
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		s++;
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
