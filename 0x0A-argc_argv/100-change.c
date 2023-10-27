3fd13f3a3bd3a5ebe4a149642fef91bf05e8c6e8
RudzaniMatidze <rdzmatidze@gmail.com>
2023-10-27 15:37:36 -0700

    5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,54 +1,17 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
 
 /**
- * coinConverter - program that converts to coins
- * @w: Passed in variable from main for calculations
- * Return: Always 0
- */
-int coinConverter(int w)
-{
-	int count = 0;
-
-	while (w != 0)
-	{
-		if (w % 10 == 9 || w % 10 == 7)
-			w -= 2;
-		else if (w % 25 == 0)
-			w -= 25;
-		else if (w % 10 == 0)
-			w -= 10;
-		else if (w % 5 == 0)
-			w -= 5;
-		else if (w % 2 == 0)
-		{
-			if (w % 10 == 6)
-				w -= 1;
-			else
-				w -= 2;
-		}
-		else
-			w -= 1;
-		count++;
-	}
-
-	return (count);
-}
-
-/**
- * main - program that prints the minimum number of coins
- * @argc: argument count
- * @argv: argument vector
- * Return: Always 0
+ * main - Prints the minimum number of coins.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If the number of arguments is not exactly one - 1.
+ *         Otherwise - 0.
  */
 int main(int argc, char *argv[])
 {
-	int w, coin;
-
-	coin = 0;
+	int cents, coins = 0;
 
 	if (argc != 2)
 	{
@@ -56,17 +19,35 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	w = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
-	if (w < 0)
-		printf("0\n");
-
-	else
+	while (cents > 0)
 	{
-		coin = coinConverter(w);
-
-		printf("%d\n", coin);
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		cents--;
 	}
+
+	printf("%d\n", coins);
 
 	return (0);
 }
