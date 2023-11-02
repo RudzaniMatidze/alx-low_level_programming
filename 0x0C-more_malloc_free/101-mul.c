947335fe4a97a85b1f5616ab1a59d02b45df7b11
RudzaniMatidze <rdzmatidze@gmail.com>
2023-11-02 15:50:20 -0700

    Write a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,92 +1,98 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
- * _puts - prints a string, followed by a new line,
- * @str: string to print
- * Return: void
-*/
-
-
-void _puts(char *str)
-{
-int x = 0;
-while (str[x])
-{
-	_putchar(str[x]);
-	x++;
-}
-
-}
-
-/**
- * _atoi - convert a string to an integer.
+ * is_digit - checks for non-digit char
  * @s: string
- * Return: integer converted
- */
-
-int _atoi(const char *s)
-{
-	int sign = 1;
-	unsigned long int resp = 0, num1, x;
-
-	for (num1 = 0; !(s[num1] >= 48 && s[num1] <= 57); num1++)
-	{
-		if (s[num1] == '-')
-		{
-			sign *= -1;
-		}
-	}
-
-	for (x = num1; s[x] >= 48 && s[x] <= 57; x++)
-	{
-		resp *= 10;
-		resp += (s[x] - 48);
-	}
-
-	return (sign * resp);
-}
-
-/**
- * print_int - prints an integer.
- * @n: integer
- * Return: always 0
- */
-
-void print_int(unsigned long int n)
-{
-
-unsigned  long int div = 1, x, resp;
-
-for (x = 0; n / div > 9; x++, div *= 10)
-;
-
-for (; div >= 1; n %= div, div /= 10)
-{
-	resp = n / div;
-	_putchar('0' + resp);
-}
-
-}
-
-/**
- * main - print the result of the multiplication, followed by a new line
- * @argc: argument count
- * @argv: argument vector
  * Return: Always 0
  */
-
-int main(int argc, char const *argv[])
+int is_digit(char *s)
 {
-(void)argc;
+	int x = 0;
 
-if (argc != 3)
+	while (s[x])
+	{
+		if (s[x] < '0' || s[x] > '9')
+			return (0);
+		x++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - returns length of string.
+ * @s: string.
+ * Return: length of string.
+ */
+int _strlen(char *s)
 {
-	_puts("Error ");
+	int x = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+	return (x);
+}
+
+/**
+ * errors - handles errors in main.
+ * Return: void
+ */
+void errors(void)
+{
+	printf("Error\n");
 	exit(98);
 }
-print_int(_atoi(argv[1]) * _atoi(argv[2]));
-_putchar('\n');
 
-return (0);
+/**
+ * main - multiplies 2 positive numbers.
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	char *str1, *str2;
+	int len1, len2, len, x, carry, digit1, digit2, *result, a = 0;
+
+	str1 = argv[1], str2 = argv[2];
+	if (argc != 3 || !is_digit(str1) || !is_digit(str2))
+		errors();
+	len1 = _strlen(str1);
+	len2 = _strlen(str2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (x = 0; x <= len1 + len2; x++)
+		result[x] = 0;
+	for (len1 = len1 - len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = str1[len1] - '0';
+		carry = 0;
+		for (len2 = strlen(str2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = str2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (x = 0; x < len - 1; x++)
+	{
+		if (result[x])
+			a = 1;
+		if (a)
+			_putchar(result[x] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
 }
