b7b1da9da2de7996589582c9a1fa406e5bc71240
RudzaniMatidze <rdzmatidze@gmail.com>
2023-11-02 14:40:30 -0700

    Write a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,38 +1,92 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 /**
- * main - multiplies two positive numbers
+ * _puts - prints a string, followed by a new line,
+ * @str: string to print
+ * Return: void
+*/
+
+
+void _puts(char *str)
+{
+int x = 0;
+while (str[x])
+{
+	_putchar(str[x]);
+	x++;
+}
+
+}
+
+/**
+ * _atoi - convert a string to an integer.
+ * @s: string
+ * Return: integer converted
+ */
+
+int _atoi(const char *s)
+{
+	int sign = 1;
+	unsigned long int resp = 0, num1, x;
+
+	for (num1 = 0; !(s[num1] >= 48 && s[num1] <= 57); num1++)
+	{
+		if (s[num1] == '-')
+		{
+			sign *= -1;
+		}
+	}
+
+	for (x = num1; s[x] >= 48 && s[x] <= 57; x++)
+	{
+		resp *= 10;
+		resp += (s[x] - 48);
+	}
+
+	return (sign * resp);
+}
+
+/**
+ * print_int - prints an integer.
+ * @n: integer
+ * Return: always 0
+ */
+
+void print_int(unsigned long int n)
+{
+
+unsigned  long int div = 1, x, resp;
+
+for (x = 0; n / div > 9; x++, div *= 10)
+;
+
+for (; div >= 1; n %= div, div /= 10)
+{
+	resp = n / div;
+	_putchar('0' + resp);
+}
+
+}
+
+/**
+ * main - print the result of the multiplication, followed by a new line
  * @argc: argument count
  * @argv: argument vector
  * Return: Always 0
-**/
+ */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-	unsigned long mul;
-	int x, y;
+(void)argc;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+if (argc != 3)
+{
+	_puts("Error ");
+	exit(98);
+}
+print_int(_atoi(argv[1]) * _atoi(argv[2]));
+_putchar('\n');
 
-	for (x = 1; x < argc; x++)
-	{
-		for (y = 0; argv[x][y] != '\0'; y++)
-		{
-			if (argv[x][y] > 57 || argv[x][y] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
-		}
-	}
-	mul = atol(argv[1]) * atol(argv[2]);
-	printf("%lu\n", mul);
-	return (0);
+return (0);
 }
