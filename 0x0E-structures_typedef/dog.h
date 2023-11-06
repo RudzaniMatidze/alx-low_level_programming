78e566ccf12ce5df5ff90449a3ef3e159aa38c5c
RudzaniMatidze <rdzmatidze@gmail.com>
2023-11-06 06:17:21 -0800

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - creates a dog info
+ * @name: name of the dog
+ * @age: age of the dog
+ * #owner: name of dog owner
+ * Desc: first struct wit alx
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+
