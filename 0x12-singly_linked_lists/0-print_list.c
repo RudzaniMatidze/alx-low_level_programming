f44231a7f5a65e5d97d6bb9944f080a0e52936c4
RudzaniMatidze <rdzmatidze@gmail.com>
2023-11-16 12:31:51 -0800

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,14 +12,12 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
-
 		else
-			printf("[%ld] %s\n", h->len, h->str);
-
-		nds++;
+			printf("[%lu] %s\n", h->len, h->str);
 		h = h->next;
+		nds++;
 	}
 	return (nds);
 }
