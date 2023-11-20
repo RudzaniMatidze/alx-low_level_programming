4ac8d814f4640e0f63ecc59cfca541970c504e60
RudzaniMatidze <rdzmatidze@gmail.com>
2023-11-20 09:02:21 -0800

    1. List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * listint_len - returns num of elements in linked listint_t
+ * @h: head pointer
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t cnt;
+
+	if (h == NULL)
+		return (0);
+	for (cnt = 0; h != NULL; cnt++)
+	{
+		h = h->next;
+	}
+	return (cnt);
+}
