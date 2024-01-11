3c1f5e1ead99d0e1a2b7a6f110811bf3d144df3f
RudzaniMatidze <rdzmatidze@gmail.com>
2024-01-11 09:07:56 -0800

    palindrome code using python

---
+++ 0x17-doubly_linked_lists/102-result.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+palindrome = 0
+
+for a in range(100, 1000):
+    for b in range(100, 1000):
+        product = a * b
+        if str(product) == str(product)[::-1] and product > largest_palindrome:
+            largest_palindrome = product
+
+# Save the result in the file
+with open("102-result", "w") as file:
+    file.write(str(largest_palindrome))
