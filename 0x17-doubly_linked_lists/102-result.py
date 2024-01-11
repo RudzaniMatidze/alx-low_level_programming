85e299c2d42161ee32730d85ee13d55dfc58a529
RudzaniMatidze <rdzmatidze@gmail.com>
2024-01-11 10:47:58 -0800

    Find the largest palindrome made from the product of two 3-digit numbers.

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -2,12 +2,13 @@
 
 largest_palindrome = 0
 
-for a in range(100, 1000):
-    for b in range(100, 1000):
-        product = a * b
-        if str(product) == str(product)[::-1] and product > largest_palindrome:
-            largest_palindrome = product
+for i in range(999, 99, -1):
+    for j in range(i, 99, -1):
+        product = i * j
+        if str(product) == str(product)[::-1]:
+            if product > largest_palindrome:
+                largest_palindrome = product
 
-# Save the result in the file
 with open("102-result", "w") as file:
     file.write(str(largest_palindrome))
+
