4bfc188b584a80ac332388ab97803b0b15c7dab4
RudzaniMatidze <rdzmatidze@gmail.com>
2024-05-08 13:56:59 -0700

    a function that searches for a value in a sorted array of integers.

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,60 @@
+#include "search_algos.h"
+
+int binary_search_recursion(int *array, int value,
+			    size_t low, size_t high);
+
+/**
+ * binary_search_recursion - helper to `advanced_binary`, recursively searches
+ * @array: Array of integers
+ * @value: value to search for
+ * @low: starting index in array
+ * @high: ending index in array
+ * Return: index with `value`, or -1 if not found or
+ */
+int binary_search_recursion(int *array, int value,
+			    size_t low, size_t high)
+{
+	size_t mid, i;
+
+	if (!array)
+		return (-1);
+
+	mid = (low + high) / 2;
+	printf("Searching in array: ");
+	for (i = low; i <= high; i++)
+		printf("%i%s", array[i], i == high ? "\n" : ", ");
+
+	if (array[low] == value)
+		return ((int)low);
+
+	if (array[low] != array[high])
+	{
+		if (array[mid] < value)
+			return (binary_search_recursion(array, value,
+							mid + 1, high));
+		if (array[mid] >= value)
+			return (binary_search_recursion(array, value,
+							low, mid));
+	}
+
+	return (-1);
+}
+
+/**
+ * advanced_binary - searches for a value in a sorted array of integers
+ * @array: Array of integers
+ * @size: Size of array
+ * @value: value to search for
+ * Return: index with `value`, or -1 if not found or
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t low = 0;
+	size_t high = size - 1;
+
+	if (!array)
+		return (-1);
+
+	return (binary_search_recursion(array, value, low, high));
+}
